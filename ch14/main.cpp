3e3c75c98c01ebbc62259997f7889a5ccf4673a4
BuiNhienLoc <80610962+BuiNhienLoc@users.noreply.github.com>
2021-03-14 11:20:52 +0700

    Add files via upload

---
+++ ch14/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    auto c = 'a'; // char type
+    auto x = 123; // int type
+    auto d = 123.456 / 789.10; // double
+    auto& y = x; // y is of int& type
+    const auto z = 123; // z is of const int type
+    return 0;
+}
